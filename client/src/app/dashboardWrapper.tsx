69eb51271c72441f0e74ec75cffa881129d0a5df
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-06-25 00:09:53 +1000

    Add Navbar, Dashboard, and DashboardWrapper components; update layout and home page to integrate them

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "@/app/(components)/Navbar";
+const DahsboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='flex bg-gray-50 text-gray-900 w-full min-h-screen'>
+      Sidebar
+      <main className='flex flex-col w-full h-fulll py-7 px-9 bg-gray-200 md:pl-24'>
+        <Navbar />
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default DahsboardWrapper;
