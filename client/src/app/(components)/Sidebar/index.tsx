5bf6d3ca2d5b63b0940552217805d7391d95aaba
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-06-26 19:10:40 +1000

    Implement Sidebar component and clean up DashboardWrapper layout

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,7 +1,31 @@
+"use client";
+import { Menu } from "lucide-react";
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const Sidebar = () => {
+  return (
+    <div>
+      <div className='flex gap-3 justify-between items-center pt-8 md:justify-normal'>
+        {/* LOGO part */}
+        {/* Sidebar align to the left when larger than md */}
+        <div>logo placeholder</div>
+        <h1 className='font-extrabold text-2xl'>STOCKX</h1>
+        <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => {}}>
+          {/* Menu icon to hide when larger than md */}
+          <Menu className='w-4 h-4' />
+        </button>
+      </div>
+
+      {/* SIDEBAR LINKS */}
+
+      <div className='flex-grow mt-8'>{/* links here  link to not grow when page size grow*/}</div>
+
+      {/* FOOTER*/}
+      <div>
+        <p className='text-center text-xs text-gray-500'>&copy; 2024 STOCKX</p>
+      </div>
+    </div>
+  );
 };
 
-export default index;
+export default Sidebar;
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -7,7 +7,6 @@ const DahsboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex bg-gray-50 text-gray-900 w-full min-h-screen'>
       {/* Side bar part */}
-
       <Sidebar />
       <main className='flex flex-col w-full h-fulll py-7 px-9 bg-gray-50 md:pl-24'>
         {/* Main part including the navbar and dashboard main content */}
