274cf7c9c511bb409ef20a5a21291ce8c92dcde6
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-06-26 17:21:48 +1000

    Add Sidebar component and update DashboardWrapper layout

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,11 +1,16 @@
 // "use client";
+// Dashboard wrapper is for layout
 import React from "react";
 import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
 const DahsboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex bg-gray-50 text-gray-900 w-full min-h-screen'>
-      Sidebar
-      <main className='flex flex-col w-full h-fulll py-7 px-9 bg-gray-200 md:pl-24'>
+      {/* Side bar part */}
+
+      <Sidebar />
+      <main className='flex flex-col w-full h-fulll py-7 px-9 bg-gray-50 md:pl-24'>
+        {/* Main part including the navbar and dashboard main content */}
         <Navbar />
         {children}
       </main>
