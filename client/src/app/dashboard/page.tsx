e81a80fb228adf9d6c50524ea7245d5701223ee4
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-06-27 22:34:43 +1000

    feat: update Dashboard component layout and add responsive grid styling

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
 export default function Dashboard() {
-  return <>Dashboard</>;
+  return (
+    <>
+      <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows'>
+        <div className='bg-amber-300 row-span-3 xl:row-span-6'>A</div>
+        <div className='bg-red-400 row-span-3 xl:row-span-6'>B</div>
+        <div className='bg-blue-400 row-span-3 xl:row-span-6'>C</div>
+      </div>
+    </>
+  );
 }
