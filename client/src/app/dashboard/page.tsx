92f355af7e5b234fcff9ab9e965f08251a7d76fd
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-06-27 22:48:30 +1000

    fix: adjust grid layout for Dashboard component to improve responsiveness

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -4,7 +4,11 @@ export default function Dashboard() {
       <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows'>
         <div className='bg-amber-300 row-span-3 xl:row-span-6'>A</div>
         <div className='bg-red-400 row-span-3 xl:row-span-6'>B</div>
-        <div className='bg-blue-400 row-span-3 xl:row-span-6'>C</div>
+        <div className='bg-blue-400 row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1'>C</div>
+        <div className='bg-black row-span-3'>D</div>
+        <div className='bg-green-400 md:row-span-1 xl:row-span-2'>E</div>
+        <div className='bg-green-900 md:row-span-1 xl:row-span-2'>F</div>
+        <div className='bg-amber-800 md:row-span-1 xl:row-span-2'>G</div>
       </div>
     </>
   );
