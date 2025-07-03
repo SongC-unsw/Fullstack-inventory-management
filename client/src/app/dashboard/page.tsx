392a2966e6b2f1a0b658a3491a7153204d09ff47
SONGYU CHEN <songyu.chen@student.unsw.edu.au>
2025-07-03 23:50:43 +1000

    feat: add CardPurchaseSummary and CardSalesSummary components to display purchase and sales metrics in the dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import CardPopularProducts from "./CardPopularProducts";
+import CardPurchaseSummary from "./CardPurchaseSummary";
+import CardSalesSummary from "./CardSalesSummary";
 
 export default function Dashboard() {
   return (
@@ -11,9 +13,11 @@ export default function Dashboard() {
 
         <div className="bg-red-400 row-span-3 xl:row-span-6">
           {/* Sales Summary */}
+          <CardSalesSummary />
         </div>
-        <div className="bg-blue-400 row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1">
+        <div className="row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1">
           {/* Purchase Summary */}
+          <CardPurchaseSummary />
         </div>
         <div className="bg-black row-span-3">D</div>
         <div className="bg-green-400 md:row-span-1 xl:row-span-2">E</div>
